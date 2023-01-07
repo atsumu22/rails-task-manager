0a7a77c33cb0c55699f0860d8b573161b03541d2
Junsuke Watanabe <w.atsumu27@gmail.com>
2023-01-07 15:38:11 +0900

    updated many things

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # # Read All
+  # get "tasks", to: "tasks#index", as: :index
+  # # Create
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
+  # # Read One
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # # Update
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  # patch "tasks/:id", to: "tasks#update"
+  # # Delete
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
 end
